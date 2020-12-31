6486f14b51211931a1713afeb524e1e445798a58
Leonardo <oliveirabalsa2@gmail.com>
2020-12-31 18:44:29 -0300

    chore: add structure to run project

---
+++ nodis-app-challenge/src/views/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NotFoundContainer = styled.div`
+    border-left: 10px solid #006c90;
+    padding: 15px;
+    background: #006c9025;
+    margin-bottom: 40px;
+
+    border-left: 12px solid#cb2d3e;
+    background:#cb2d3e25;
+`
+
+export const Title = styled.div`
+    margin: 0;
+    font-weight: 600;
+    font-size: 1.4rem
+`
+export const Text = styled.div`
+`
