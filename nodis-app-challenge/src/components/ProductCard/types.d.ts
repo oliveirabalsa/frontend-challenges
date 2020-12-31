378362ba5b8808e2cff0fd6330432ac0d970cbab
Leonardo <oliveirabalsa2@gmail.com>
2020-12-31 18:47:35 -0300

    feat: add types for product card component props

---
+++ nodis-app-challenge/src/components/ProductCard/types.d.ts
@@ -0,0 +1,10 @@
+export interface ProductCardProps {
+    imageUrl: string;
+    name: string;
+    promotionalPrice: string;
+    salePrice: string;
+    value: string;
+    cents: string;
+    promotionalValue: string;
+    promotionalCents: string;
+}
