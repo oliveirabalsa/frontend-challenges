83ef71aaac79cc449b2b688a347a661450967972
Leonardo <oliveirabalsa2@gmail.com>
2020-12-31 18:48:20 -0300

    feat: add Apollo provider to App

---
+++ nodis-app-challenge/src/views/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Content from '../components/layout/Content';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../index'
+import "./App.css";
+
+const App: React.FC = (props) => (
+  <ApolloProvider client={client}>
+     <Router>
+      {/* <ProductDetails /> */}
+      <Content />
+    </Router>
+  </ApolloProvider>
+   
+);
+
+export default App;
