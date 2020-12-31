3c684d4e4a3ca4682aab3d5efc854b7d4baf33cf
Leonardo <oliveirabalsa2@gmail.com>
2020-12-31 18:46:08 -0300

    feat: add home structure

---
+++ nodis-app-challenge/src/components/Home/Home.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Title } from "./styles";
+import { useQuery, gql } from "@apollo/client";
+
+import ProductDetails from "../../views/pages/ProductDetails/ProductDetails";
+import ProductCard from "../ProductCard/ProductCard";
+
+const GET_PRODUCTS = gql`
+  query {
+    allSkus {
+      id
+      name
+      ean
+      category
+      imageUrl
+      description
+      salePrice
+      promotionalPrice
+      quantity
+    }
+  }
+`;
+
+const convertPrice = (price: string) => {
+  price = price.toString();
+
+  const priceLength = price.length;
+
+  return [price.slice(0, price.length - 2), price.slice(priceLength - 2)];
+};
+
+const handleProducts = (data: any) => {
+  const response = [];
+  for (let product of data) {
+    let price = product.salePrice;
+    let promotionalPrice = product.promotionalPrice;
+
+    const [value, cents] = convertPrice(price);
+    const [promotionalValue, promotionalCents] = convertPrice(promotionalPrice);
+
+    const productParsed = {
+      ...product,
+      value,
+      cents,
+      promotionalValue,
+      promotionalCents,
+    };
+
+    response.push(<ProductCard {...productParsed} />);
+  }
+  return response;
+};
+
+const Home: React.FC = (props) => {
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  return (
+    <div className="Home">
+      <Title>Produtos</Title>
+
+      {loading ? <h1>Loading...</h1> : handleProducts(data.allSkus)}
+    </div>
+  );
+};
+
+export default Home;
