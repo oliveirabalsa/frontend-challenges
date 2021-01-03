49c0a49977669e2eaa0241caa4c06d4859745e3c
Leonardo <oliveirabalsa2@gmail.com>
2021-01-03 12:54:14 -0300

    fix: change structure to index

---
+++ app/src/views/pages/ProductDetails/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductDetails from "../../../components/ProductDetails";
+
+const ProductDetailsPage: React.FC = (props) => {
+  return(
+    <ProductDetails />
+  )
+}
+
+export default ProductDetailsPage;
