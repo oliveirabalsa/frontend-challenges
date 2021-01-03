dc1c4104e3bc255941ad8f152d34e7341237b463
Leonardo <oliveirabalsa2@gmail.com>
2021-01-03 12:49:41 -0300

    feat: implement home styles

---
+++ app/src/components/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Title = styled.p`
+  font-size: 21px;
+  line-height: 24px;
+  color: rgba(51, 51, 51, 1);
+  padding: 33px 20px 20px 12px; 
+`;
