3cd8affa7ee1bf1d977535684012c63483516dfd
Leonardo <oliveirabalsa2@gmail.com>
2021-01-03 12:51:37 -0300

    feat: implement types for product props

---
+++ app/src/components/ProductCard/types.d.ts
@@ -0,0 +1,11 @@
+export interface ProductCardProps {
+    id: string;
+    imageUrl: string;
+    name: string;
+    promotionalPrice: string;
+    salePrice: string;
+    value: string;
+    cents: string;
+    promotionalValue: string;
+    promotionalCents: string;
+}
